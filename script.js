2d869b9d51fd55de9235f1446b65fa968952bb7c
Emmylrd <36962728+Emmylrd@users.noreply.github.com>
2019-12-18 09:11:32 +0100

    Prendre en compte les requêtes en minuscule

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var singerArr=[];
 $( document ).ready(function() {
     function loadAllSingers(){
 		
-         var singerListSPARQL = "prefix foaf: <http://xmlns.com/foaf/0.1/> prefix dbo: <http://dbpedia.org/ontology/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select distinct ?name Where { ?x rdf:type dbo:MusicalArtist. ?x foaf:name ?name. ?x dbo:birthDate ?birth. filter(?birth >= \"19000101\"^^xsd:date)}";
+         var singerListSPARQL = "prefix foaf: <http://xmlns.com/foaf/0.1/> prefix dbo: <http://dbpedia.org/ontology/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select distinct ?name Where { ?x rdf:type dbo:MusicalArtist. ?x foaf:name ?name. ?x dbo:birthDate ?birth. filter(?birth >= \"19600101\"^^xsd:date)}";
          var progress=10;
          //Preparing SPARQL query against DBPedia
          var singerNameQuery = "http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=" + escape(singerListSPARQL) + "&format=json";
@@ -48,7 +48,7 @@ $( document ).ready(function() {
     $( "#research" ).click(function(e) {
 	
       event.preventDefault();  
-      researchSinger($( "#singers" ).val() );
+      researchSinger($("#singers").val().charAt(0).toUpperCase() + $("#singers").val().substring(1).toLowerCase());
       var res = $( "#singers" ).val() ;
       res = res.replace(" ", "_");
       console.log(res);
@@ -94,7 +94,7 @@ $( document ).ready(function() {
 	}
 
     function researchSinger(singerName) {
-         var singerSPARQL = "prefix foaf: <http://xmlns.com/foaf/0.1/> prefix dbo: <http://dbpedia.org/ontology/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select distinct ?name, ?birthName,?birthDate,?deathDate,str(?birthPlace),?spouse,?picture,?description,?gender Where { ?x rdf:type dbo:MusicalArtist. ?x foaf:name ?name.  filter contains(?name,\""+ singerName +"\"). optional{?x foaf:gender ?gender} .optional{?x dbo:birthName ?birthName } optional{?x dbo:birthDate ?birthDate FILTER ( ?birthDate >= \"19000101\"^^xsd:date && ?birthDate <= \"20991231\"^^xsd:date ) } optional{?x dbo:deathDate ?deathDate  FILTER ( ?deathDate >= \"19000101\"^^xsd:date && ?deathDate <= \"20991231\"^^xsd:date )} optional{?x dbo:birthPlace ?birthPlace } optional{?x dbo:spouse ?spouse} optional{?x dbo:thumbnail ?picture} optional{?x dct:description ?description} }";
+         var singerSPARQL = "prefix foaf: <http://xmlns.com/foaf/0.1/> prefix dbo: <http://dbpedia.org/ontology/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select distinct ?name, ?birthName,?birthDate,?deathDate,str(?birthPlace),?spouse,?picture,?description Where { ?x rdf:type dbo:MusicalArtist. ?x foaf:name ?name.  filter contains(?name,\""+ singerName +"\"). optional{?x dbo:birthName ?birthName } optional{?x dbo:birthDate ?birthDate FILTER ( ?birthDate >= \"19000101\"^^xsd:date && ?birthDate <= \"20991231\"^^xsd:date ) } optional{?x dbo:deathDate ?deathDate  FILTER ( ?deathDate >= \"19000101\"^^xsd:date && ?deathDate <= \"20991231\"^^xsd:date )} optional{?x dbo:birthPlace ?birthPlace } optional{?x dbo:spouse ?spouse} optional{?x dbo:thumbnail ?picture} optional{?x dct:description ?description} }";
          var singerNameQuery = "http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=" + escape(singerSPARQL) + "&format=json&timeout=3000000";
          
          $.ajax({
@@ -109,18 +109,9 @@ $( document ).ready(function() {
                     $('#tableResult').append('<tr><td>Nom </td><td>'+ data.results.bindings[0].name.value +'</td></tr>');
                 }
 				
-				if(data.results.bindings[0].gender != null){
-					if(data.results.bindings[0].gender.value=='female'){
-						$('#tableResult').append('<tr><td>Genre </td><td>'+'Femme'+'</td></tr>');
-					}else if(data.results.bindings[0].gender.value=='male'){
-						$('#tableResult').append('<tr><td>Genre </td><td>'+'Homme'+'</td></tr>');
-					} 
-				}
-				
 				if(data.results.bindings[0].description != null){
                     $('#tableResult').append('<tr><td>Déscription </td><td>'+ data.results.bindings[0].description.value +'</td></tr>');
                 }
-								
 
                 if(data.results.bindings[0].birthName != null){
                     $('#tableResult').append('<tr><td>Nom de naissance </td><td>'+ data.results.bindings[0].birthName.value +'</td></tr>');
@@ -151,14 +142,6 @@ $( document ).ready(function() {
                     $('#tableResult4').append('<tr><td>Nom </td><td>'+ data.results.bindings[1].name.value +'</td></tr>');
 					}
 				
-				    if(data.results.bindings[1].gender != null){
-						if(data.results.bindings[1].gender.value=='female'){
-							$('#tableResult4').append('<tr><td>Genre </td><td>'+'Femme'+'</td></tr>');
-						}else if(data.results.bindings[1].gender.value=='male'){
-							$('#tableResult4').append('<tr><td>Genre </td><td>'+'Homme'+'</td></tr>');
-						} 
-					}
-								
 					if(data.results.bindings[1].description != null){
 						$('#tableResult4').append('<tr><td>Déscription </td><td>'+ data.results.bindings[1].description.value +'</td></tr>');
 					}
@@ -200,7 +183,7 @@ $( document ).ready(function() {
 	 }
          
          function researchAlbum(singerName) {
-         var singerSPARQL = "prefix foaf: <http://xmlns.com/foaf/0.1/> prefix dbo: <http://dbpedia.org/ontology/> prefix dbp: <http://dbpedia.org/property/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select distinct ?name ?artist ?releaseDate Where { ?x rdf:type dbo:MusicalWork. ?x dbo:artist ?artist. filter contains(str(?artist),\""+ singerName +"\"). ?x dbp:thisAlbum ?name. Optional{?x dbo:releaseDate ?releaseDate}.}Limit 5.";
+         var singerSPARQL = "prefix foaf: <http://xmlns.com/foaf/0.1/> prefix dbo: <http://dbpedia.org/ontology/> prefix dbp: <http://dbpedia.org/property/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select distinct ?name ?artist ?releaseDate Where { ?x rdf:type dbo:MusicalWork. ?x dbo:artist ?artist. filter contains(str(?artist),\""+ singerName +"\"). ?x dbp:thisAlbum ?name. Optional{?x dbo:releaseDate ?releaseDate}. }";
          var singerNameQuery = "http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=" + escape(singerSPARQL) + "&format=json";    
          console.log(singerSPARQL);     
          $.ajax({
@@ -236,7 +219,7 @@ $( document ).ready(function() {
          
     function researchSong(singerName) {
 	     var Song = "Song";
-         var singerSPARQL = "prefix foaf: <http://xmlns.com/foaf/0.1/> prefix dbo: <http://dbpedia.org/ontology/> prefix dbp: <http://dbpedia.org/property/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select distinct ?name ?album Where { ?x rdf:type dbo:MusicalWork. ?x dbo:artist ?artist. filter contains(str(?artist),\""+ singerName +"\"). ?x rdf:type ?type. ?x foaf:name ?name. filter contains(str(?type),\"Song\"). ?x dbo:album ?album. }Limit 5.";
+         var singerSPARQL = "prefix foaf: <http://xmlns.com/foaf/0.1/> prefix dbo: <http://dbpedia.org/ontology/> prefix dbp: <http://dbpedia.org/property/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select distinct ?name ?album Where { ?x rdf:type dbo:MusicalWork. ?x dbo:artist ?artist. filter contains(str(?artist),\""+ singerName +"\"). ?x rdf:type ?type. ?x foaf:name ?name. filter contains(str(?type),\"Song\"). ?x dbo:album ?album. }Limit 10.";
          var singerNameQuery = "http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=" + escape(singerSPARQL) + "&format=json";    
          console.log(singerSPARQL);     
          $.ajax({
